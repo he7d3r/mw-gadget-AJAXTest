6a6f5e4fc258a0b2a713600c8380abaaafc2999e
Helder <he7d3r@gmail.com>
2014-09-27 20:48:55 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/AJAXTest.js
+++ src/AJAXTest.js
@@ -5,71 +5,71 @@
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 
-function editPage( info ) {
-	// Edit page (must be done through POST)
-        $.ajax({
-                url: mw.util.wikiScript( 'api' ),
-                type: 'POST',
-                dataType: 'json',
-                data: {
-                        format: 'json',
-                        action: 'edit',
-                        title: info.title,
-                        text: info.text,
-                        summary: info.summary,
-                        token: mw.user.tokens.get( 'editToken' )
-                },
-                success: function( data ) {
-                        if ( data && data.edit && data.edit.result && data.edit.result === 'Success' ) {
-                                alert( 'Page edited!' );
-                        } else {
-                                alert( 'The edit query returned an error. =(' );
-                        }
-                },
-                error: function() {
-                        alert( 'The ajax request failed.' );
-                }
-        });
-}
-function getTextFromSpecialBook() {
-	var	bookName = $( '#titleInput' ).val(),
-		text = ';(Este seria o índice criado para o livro "' +
-			bookName + '")\n\n== Índice ==\n* [[/',
-		$chapters = $( '#collectionList' ).find( 'strong' ),
-		list = [];
-
-	if ( !$chapters.length ) {
-		alert( 'É preciso definir os nomes dos capítulos!' );
-		return;
+	function editPage( info ) {
+		// Edit page (must be done through POST)
+		$.ajax({
+			url: mw.util.wikiScript( 'api' ),
+			type: 'POST',
+			dataType: 'json',
+			data: {
+				format: 'json',
+				action: 'edit',
+				title: info.title,
+				text: info.text,
+				summary: info.summary,
+				token: mw.user.tokens.get( 'editToken' )
+			},
+			success: function ( data ) {
+				if ( data && data.edit && data.edit.result && data.edit.result === 'Success' ) {
+					alert( 'Page edited!' );
+				} else {
+					alert( 'The edit query returned an error. =(' );
+				}
+			},
+			error: function () {
+				alert( 'The ajax request failed.' );
+			}
+		});
 	}
-	$chapters.each( function( ){
-		list.push( $( this ).text() );
-	} );
-	text += list.join( '/]] [[Imagem:00%.svg]]\n* [[/' ) +
-		'/]] [[Imagem:00%.svg]]\n\n{' +
-		'{Fases5}}\n{' + '{AutoCat}}\n{' +
-		'{Ordem alfabética|' + bookName.substr(0,1) + '}}';
-	return text;
-}
+	function getTextFromSpecialBook() {
+		var	bookName = $( '#titleInput' ).val(),
+			text = ';(Este seria o índice criado para o livro "' +
+				bookName + '")\n\n== Índice ==\n* [[/',
+			$chapters = $( '#collectionList' ).find( 'strong' ),
+			list = [];
 
-if ( mw.config.get('wgCanonicalSpecialPageName') === 'Book' ) {
-	$(function () {
-		$( mw.util.addPortletLink(
-			'p-cactions',
-			'#',
-			'Criar livro (AJAX)',
-			'ca-ajax-edit'
-		)).click( function( e ) {
-			e.preventDefault();
-			editPage({
-				title: 'User:' + mw.config.get('wgUserName') + '/AJAX',
-				text: getTextFromSpecialBook(),
-				summary: 'Trying to edit my sandbox using AJAX...'
+		if ( !$chapters.length ) {
+			alert( 'É preciso definir os nomes dos capítulos!' );
+			return;
+		}
+		$chapters.each( function ( ) {
+			list.push( $( this ).text() );
+		} );
+		text += list.join( '/]] [[Imagem:00%.svg]]\n* [[/' ) +
+			'/]] [[Imagem:00%.svg]]\n\n{' +
+			'{Fases5}}\n{' + '{AutoCat}}\n{' +
+			'{Ordem alfabética|' + bookName.substr(0, 1) + '}}';
+		return text;
+	}
+
+	if ( mw.config.get('wgCanonicalSpecialPageName') === 'Book' ) {
+		$(function () {
+			$( mw.util.addPortletLink(
+				'p-cactions',
+				'#',
+				'Criar livro (AJAX)',
+				'ca-ajax-edit'
+			)).click( function ( e ) {
+				e.preventDefault();
+				editPage({
+					title: 'User:' + mw.config.get('wgUserName') + '/AJAX',
+					text: getTextFromSpecialBook(),
+					summary: 'Trying to edit my sandbox using AJAX...'
+				});
 			});
 		});
-	});
-}
+	}
 
 }( mediaWiki, jQuery ) );
